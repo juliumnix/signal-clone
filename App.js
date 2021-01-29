a7c5c55989f457b647466561fad633b31957fa7d
juliumnix <juliumnix@gmail.com>
2021-01-29 10:44:47 -0300

    adicionada animação ios

--- App.js
+++ App.js
@@ -3,20 +3,40 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack'
+import {CardStyleInterpolators, createStackNavigator} from '@react-navigation/stack'
 
 import RegisterScreen from './screens/RegisterScreen'
 import LoginScreen from "./screens/LoginScreen"
 
 const Stack = createStackNavigator();
 
+// const config = {
+//   animation: 'spring',
+//   config: {
+//     stiffness: 1000,
+//     damping: 500,
+//     mass: 3,
+//     overshootClamping: true,
+//     restDisplacementThreshold: 0.01,
+//     restSpeedThreshold: 0.01,
+//   },
+// };
+
 const globalScreenOptions = {
   headerStyle: { backgroundColor: "#2C6BED" },
   headerTitleStyle: { color: "white" },
   headerTintColor: "white",
   headerTitleAlign: "center",
-  
+  gestureEnabled: true,
+  gestureDirection: 'horizontal',
+  // transitionSpec: {
+  //   open: config,
+  //   close: config,
+  // },
+  cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
 }
+
+
 
 export default function App() {
   return (
